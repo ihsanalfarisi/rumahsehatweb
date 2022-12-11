103edfab1f6092464236978f42ce650f6c20903b
Achmad Hafiz <achmad.hafiz@ui.ac.id>
2022-12-12 01:34:45 +0700

    Feat/home mobile api

---
+++ src/main/java/apap/tugasakhir/rumahsehat/model/HomeDataModel.java
@@ -0,0 +1,13 @@
+package apap.tugasakhir.rumahsehat.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@AllArgsConstructor
+@Getter
+@Setter
+public class HomeDataModel {
+    private Long saldo;
+    private Integer appointment_count;
+}
